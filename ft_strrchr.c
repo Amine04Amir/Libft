48a829c0079f42ce1f5463944e3c204378c5731e
Mohamed Amine Amir <mamir@e1r12p9.1337.ma>
2023-11-15 14:22:13 +0100

    PART 1 complete

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,21 +6,21 @@
 /*   By: mamir <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 17:17:30 by mamir             #+#    #+#             */
-/*   Updated: 2023/11/15 14:17:20 by mamir            ###   ########.fr       */
+/*   Updated: 2023/11/15 14:20:02 by mamir            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strrchr(const char *s, int c)
+char	*ft_strrchr(const char *str, int c)
 {
 	int	i;
-	
+
 	i = ft_strlen(str);
 	while (i >= 0)
 	{
 		if (str[i] == (char)c)
-			return ((char*)str + i);
+			return ((char *)str + i);
 		i--;
 	}
 	return (NULL);

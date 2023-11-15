5711b83ebcac608b5d8cc459e8f88eca7311ccf2
Mohamed Amine Amir <mamir@e1r12p9.1337.ma>
2023-11-15 16:09:12 +0100

    strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,29 +6,24 @@
 /*   By: mamir <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 17:36:25 by mamir             #+#    #+#             */
-/*   Updated: 2023/11/15 11:48:48 by mamir            ###   ########.fr       */
+/*   Updated: 2023/11/15 15:24:44 by mamir            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strdup(const char *s)
+char	*ft_strdup(const char *src)
 {
-	size_t	i;
-	size_t	len;
-	char	*dup;
+	char	*new;
+	int	i;
 
-	len = ft_strlen(s);
-	dup = (char *)malloc((len + 1) * sizeof(char));
 	i = 0;
-	if (dup == NULL)
-	{
+	if (!(new = (char *)malloc(sizeof(char) * ft_strlen(src) + 1)))
 		return (NULL);
-	}
-	while (i <= len)
+	while (*src)
 	{
-		dup[i] = s[i];
-		i++;
+		new[i++] = *src++;
 	}
-	return (dup);
+	new[i] = '\0';
+	return (new);
 }

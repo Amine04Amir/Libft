cb557482786c376124fcf07bbfaec2fa39566db5
Amine04Amir <raja_amir@outlook.fr>
2023-11-25 16:14:38 +0100

    Part 2

--- ft_split.c
+++ ft_split.c
@@ -12,14 +12,49 @@
 
 #include "libft.h"
 
-static int	count()
+static int	wordcount(char const *s, char c)
 {
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (s && s[i] != '\0')
+	{
+		if (s[i] != c)
+		{
+			count++;
+			while(s[i] && s[i] != c)
+				i++;
+		}
+		else if (s[i] == c)
+			i++;
+	}
+	return (count);
 }
-static char	*checknull(char *p, int n)
+
+static char	**free(char **str, int n)
 {
-	if (n > 0)
+	while (n > 0)
+	{
+		free(str[--n]);
+	}
+	free(str);
+	return (0);
+}
 
 char **ft_split(char const *s, char c)
 {
+	char **ptr;
+	int	i;
+	int	n;
+	int	j;
+
+	ptr = (char **)malloc((wordcount(s, c) + 1) * sizeof(char));
+	if (!ptr)
+		return (NULL);
+	i = 0;
+	n = 0;
+
 
 }

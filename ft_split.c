9e529662f01e164a22282dd4575d7397d6e7e134
Amine04Amir <raja_amir@outlook.fr>
2023-11-25 18:41:23 +0100

    Part 2

--- ft_split.c
+++ ft_split.c
@@ -12,7 +12,7 @@
 
 #include "libft.h"
 
-static int	wordcount(char const *s, char c)
+static int	ft_wordcount(char const *s, char c)
 {
 	int	i;
 	int	count;
@@ -24,7 +24,7 @@ static int	wordcount(char const *s, char c)
 		if (s[i] != c)
 		{
 			count++;
-			while(s[i] && s[i] != c)
+			while (s[i] != '\0' && s[i] != c)
 				i++;
 		}
 		else if (s[i] == c)
@@ -33,7 +33,7 @@ static int	wordcount(char const *s, char c)
 	return (count);
 }
 
-static char	**free(char **str, int n)
+static char	**free_ptr(char **str, int n)
 {
 	while (n > 0)
 	{
@@ -43,18 +43,31 @@ static char	**free(char **str, int n)
 	return (0);
 }
 
-char **ft_split(char const *s, char c)
+char	**ft_split(char const *s, char c)
 {
-	char **ptr;
-	int	i;
-	int	n;
-	int	j;
+	char	**ptr;
+	int		i;
+	int		j;
+	int		n;
 
-	ptr = (char **)malloc((wordcount(s, c) + 1) * sizeof(char));
+	ptr = (char **)malloc((ft_wordcount(s, c) + 1) * sizeof(char *));
 	if (!ptr)
 		return (NULL);
 	i = 0;
 	n = 0;
-
-
+	while (s && s[i] != '\0')
+	{
+		while (s[i] == c)
+			i++;
+		if (s[i] == '\0')
+			break ;
+		j = 0;
+		while (s[i] != c && s[i] != '\0' && j++ > -1)
+			i++;
+		ptr[n++] = ft_substr(s, i - j, j);
+		if (ptr[n - 1] == NULL)
+			return (free_ptr(ptr, n));
+	}
+	ptr[n] = NULL;
+	return (ptr);
 }
--- libft.h
+++ libft.h
@@ -48,5 +48,6 @@ char	*ft_strjoin(const char *s1, const char *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char	*ft_itoa(int n);
+char	**ft_split(char const *s, char c);
 
 #endif
